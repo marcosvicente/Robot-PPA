ef453af776e7cbcc6e519f8f33ef87191b9245f2
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2019-02-28 14:33:07 -0300

    get all h2 of index page

---
+++ robot/web_scraping.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+import urllib.request
+
+class WebScraping:
+    def __init__(self):
+        self.soup = ""
+
+    def set_url(self):
+        """get a url of site and save a varible"""
+
+        return urllib.request.urlopen("https://btv.ifsp.edu.br/")
+
+    def get_html_of_site(self):
+        """docstring for get_name_of_courses"""
+        url = self.set_url()
+        self.soup = BeautifulSoup(url, 'html.parser')
+
+    def get_name_of_courses(self):
+        """docstring for get_name_of_courses"""
+        self.get_html_of_site()
+        print(self.soup.find_all('h2'))
+
+    def web_scraping(self):
+        self.get_name_of_courses()
+
+w = WebScraping()
+w.web_scraping()
