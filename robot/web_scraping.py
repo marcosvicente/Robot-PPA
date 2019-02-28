15f0728a0389f9ec17fb8dcfcc0ea11c9bc85d4d
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2019-02-28 15:55:04 -0300

    get url and tag all courses

--- robot/web_scraping.py
+++ robot/web_scraping.py
@@ -6,25 +6,49 @@ import urllib.request
 class WebScraping:
     def __init__(self):
         self.soup = ""
+        self.url_base = "https://btv.ifsp.edu.br"
+        self.url_page_courses = ""
+        self.soup_courses = ""
+        self.url_courses = []
 
-    def set_url(self):
-        """get a url of site and save a varible"""
+    def set_url_base(self):
+        """get a url of site and save a variable"""
 
-        return urllib.request.urlopen("https://btv.ifsp.edu.br/")
+        return urllib.request.urlopen(self.url_base)
 
     def get_html_of_site(self):
-        """docstring for get_name_of_courses"""
-        url = self.set_url()
+        """get html of site and put a variable"""
+        url = self.set_url_base()
         self.soup = BeautifulSoup(url, 'html.parser')
 
-    def get_url_of_courses(self):
-        """docstring for get_name_of_courses"""
+    def get_tag_of_courses(self):
+        """get tag cursos"""
         self.get_html_of_site()
         return self.soup.find_all('a', text="Cursos")
 
+    def set_url_of_courses(self):
+        for tag in self.get_tag_of_courses():
+            url = tag.get("href")
+        return url
+
+
+    def get_page_courses(self):
+        url = "{0}{1}".format(self.url_base, self.set_url_of_courses())
+        self.url_page_courses = urllib.request.urlopen(url)
+        self.soup_courses = BeautifulSoup(self.url_page_courses, 'html.parser')
+
+
+    def get_name_of_courses(self):
+        self.get_page_courses()
+        for item_page in self.soup_courses.find_all("div", class_="item-page"):
+            for ul in item_page.find_all("ul"):
+                for a in ul.find_all("a"):
+                    self.url_courses.append(a)
+
     def web_scraping(self):
-        self.get_url_of_courses()
-        print(self.get_url_of_courses())
+        self.get_name_of_courses()
+
+        print(self.url_courses[4])
 
 w = WebScraping()
 w.web_scraping()
